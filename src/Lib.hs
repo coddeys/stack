46fa10afd7a3d3d7d48e19b2cac9f58537d7fe37
Coddeys <coddeys@gmail.com>
2015-09-09 21:18:51 +0000

    Learning Stack

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
 
-import qualified Data.Text.IO as T
+import Acme.Missiles
 
 someFunc :: IO ()
-someFunc = T.putStrLn "someFunc"
+someFunc = launchMissiles
